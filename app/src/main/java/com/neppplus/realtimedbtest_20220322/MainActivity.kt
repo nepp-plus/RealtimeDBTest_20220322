59223b24ad806b4435230a84c8c3d4635a56f5d6
nepp.plus <nepp.plus@gmail.com>
2022-03-22 10:01:56 +0900

    제일 기본 형태의 데이터 기록 체험  - DB 연결  - 항목명 설정 (test)  - 항목에 실제 값 기록 (Hello World)

--- app/src/main/java/com/neppplus/realtimedbtest_20220322/MainActivity.kt
+++ app/src/main/java/com/neppplus/realtimedbtest_20220322/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.realtimedbtest_20220322
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,17 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        DB 연결 -> 값 기록 연습.
+
+        val db = FirebaseDatabase.getInstance("https://realtimedbtest-20220322-default-rtdb.asia-southeast1.firebasedatabase.app/") // 싱가폴 DB주소 대입
+
+//        DB의 하위 항목 (Reference) 설정
+        val testRef = db.getReference("test")
+
+//        test 항목에, "Hello World!!" 기록
+
+        testRef.setValue("Hello World")
 
     }
 }
